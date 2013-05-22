6e05bf0c365df0e30b33649326888172a743d1af
René Stalder <renestalder@me.com>
2013-05-22 10:43:10 +0200

    Fix for Windows users.
    The REM seems to cause problems. I removed It since someone in the
    community reported it works perfect without.

--- compasswatch.bat
+++ compasswatch.bat
@@ -1,8 +1,8 @@
 IF EXIST %1\config.rb (
-	REM compass watch %1 --boring
+	compass watch %1 --boring
 )
 
 IF EXIST %2\config.rb (
-	REM compass watch %2 --boring
+	compass watch %2 --boring
 )
 pause
