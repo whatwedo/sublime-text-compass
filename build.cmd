241b0385d0f6b86cb920cbf4a843ca96095cdc80
Attila Beregszaszi <attila_beregszaszi@dennis.co.uk>
2013-08-23 15:09:43 +0100

    Fix build on Windows with paths containing whitespace and/or in same directory Fixed script arguments in build file for all platforms

--- build.cmd
+++ build.cmd
@@ -1,16 +1,23 @@
 @echo off
 setlocal EnableDelayedExpansion
-set fileDir=%~dp1
-:loop
-for %%F in ("%fileDir%\..") do if not exist "%fileDir%\config.rb" (
-  if %fileDir% neq %%~fF (
+set fileDir=%~f1
+
+if exist "%fileDir%\config.rb" goto :build
+
+:search
+for /f "tokens=* delims=;" %%F in ("%fileDir%\..") do if not exist "%fileDir%\config.rb" (
+  if "%fileDir%" neq "%%~fF" (
     set "fileDir=%%~fF"
-    goto :loop
-  ) else set "fileDir="
+    goto :search
+  ) else (
+    set "fileDir="
+    goto :build
+  )
 )
 
+:build
 if not defined fileDir (
   echo [ERROR] config.rb not found.
 ) else (
-  compass compile %fileDir% --boring
+  compass compile '%fileDir%' --boring
 )
